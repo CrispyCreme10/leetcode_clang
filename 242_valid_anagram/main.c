850ebedd118a6f707452f354727db4013685d781
CrispyCreme10 <jordanrenner69@gmail.com>
2023-06-15 18:36:36 -0500

    yessir

--- 242_valid_anagram/main.c
+++ 242_valid_anagram/main.c
@@ -2,14 +2,10 @@
 #include <stdbool.h>
 #include <string.h>
 
-bool isAnagram(char* s, char* t);
-
-int main() {
-  bool result1 = isAnagram("anagram", "nagaram");
-  printf("result1 is: %s\n", result1 ? "true" : "false");
-
-  bool result2 = isAnagram("rat", "car");
-  printf("result2 is: %s\n", result2 ? "true" : "false");
+void swap(char *a, char *b) {
+  char temp = *a;
+  *a = *b;
+  *b = temp;
 }
 
 bool isAnagram(char* s, char* t) {
@@ -22,25 +18,34 @@ bool isAnagram(char* s, char* t) {
     return false;
 
   for (i = 0; i < sSize; i++) {
+    // printf("t: %s\n", t);
     for (j = i; j < sSize; j++) {
       if (*(t + j) == *(s + i)) {
-        printf("found matching char %c at pos %d to init char %c at pos %d\n", *(t + j), j, *(s + i), i);
+        // printf("found matching char %c at pos %d to init char %c at pos %d\n", *(t + j), j, *(s + i), i);
         if (j > i) {
-          char temp = t[i];
-          printf("test: %c %c", temp, t[j]);
-          t[i] = &t[j];
-          printf("test: %c %c", temp, t[j]);
-          t[j] = temp;
-          printf("test: %c %c", temp, t[j]);
+          swap(&t[i], &t[j]);
         }
         charFound = true;
         break;
       }
     }
 
-    printf("t: %s\n", t);
     if (!charFound)
       return false;
     charFound = false;
   }
+
+  return true;
+}
+
+int main() {
+  char str1[] = "anagram";
+  char str2[] = "nagaram";
+  bool result1 = isAnagram(str1, str2);
+  printf("result1 is: %s\n", result1 ? "true" : "false");
+
+  char str3[] = "rat";
+  char str4[] = "car";
+  bool result2 = isAnagram(str3, str4);
+  printf("result2 is: %s\n", result2 ? "true" : "false");
 }
